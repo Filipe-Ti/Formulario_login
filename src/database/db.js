f49754db03ae9137556b4cfdb492829e22c8a124
Filipe-santos <fsj.santos20@gmail.com>
2021-10-07 21:11:47 -0300

    Ordenación en la carpeta Public y configuración del Package json

--- src/database/db.js
+++ src/database/db.js
@@ -1,3 +1,67 @@
+ // importar a depedencia do sqlite3
+const sqlite3 = require("sqlite3").verbose()
+
+ // criar o objeto que irá fazer operações no banco de dados
+const db = new sqlite3.Database("./src/database/database.db") 
+
+ // Exporta o banco de dados
+module.exports = db;
+
+ // utilizar o objeto de banco de dados, para nossas operações
+/*db.serialize(() => {
+
+  // 1 Criar uma tabela
+db.run(`
+      CREATE TABLE IF NOT EXISTS dadosUsuario(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email TEXT,
+        password NUMBER
+      );
+`)
+
+  // 2 Inserir dados na tabela
+const query =`
+   INSERT INTO dadosUsuario(
+       email,
+       password
+   ) VALUES (?,?);
+`
+
+const values = [
+  "gtr@email.com",
+  "76452390"
+]
+db.run(query, values, function(err) {
+  if(err) {
+    return console.log(err)
+  }
+
+  console.log("Cadastrado com sucesso");
+  console.log(this)
+ });
+
+
+ // 3 Consultar os dados da tabela
+ db.all(`SELECT * FROM dadosUsuario`, function(err, rows) {
+  if(err) {
+    return console.log(err)
+  }
+
+  console.log("Aqui estão seus regitros:");
+  console.log(rows);
+}) 
+
+ // 4 Deletar um dado da tabela
+//db.run(`DELETE FROM dadosUsuario WHERE id = ?`, [1], function(err) {
+//  if(err) {
+//      return console.log(err);
+//    }
+//    console.log("Registro deletado com sucesso!");
+// });
+
+});
+/*
+
 /*
 const { Sequelize } = require('sequelize');
 
--- src/server.js
+++ src/server.js
@@ -1,17 +1,15 @@
-const express = require('express');
+const express = require("express");
 const server = express();
+
+// Pegar o banco de dados
+const db = require("./database/db");
 
 // configurar pasta pública
 server.use(express.static("public"));
 
 server.get('/', (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
-});
-
-server.post('/cad', (req, res) => {
-
-});
- 
+})
 server.listen(3000, function() {
-    console.log('Ok');
+    console.log("Listening at the port 3000");
 })
